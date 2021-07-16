8c52635462ae027a4fabe9d85ef1b9deffa78eaf
Kelly Jerrell <kcjerrell@gmail.com>
2021-07-16 13:32:15 -0700

    first-migration written

---
+++ data/migrations/20210716185203_first-migration.js
@@ -0,0 +1,37 @@
+
+exports.up = async function (knex) {
+	await knex.schema
+		.createTable('projects', table => {
+			table.increments('project_id')
+			table.text('project_name').notNullable()
+			table.text('project_description')
+			table.text('project_completed').defaultTo(false)
+		})
+		.createTable('resources', table => {
+			table.increments('resource_id')
+			table.text('resource_name').unique().notNullable()
+			table.text('resource_description')
+		})
+		.createTable('tasks', table => {
+			table.increments('task_id')
+			table.text('task_description').notNullable()
+			table.text('task_notes')
+			table.integer('project_id').unsigned().notNullable()
+				.references('project_id').inTable('projects')
+		})
+		.createTable('project_resources', table => {
+			table.increments('project_resource_id')
+			table.integer('resource_id').unsigned().notNullable()
+				.references('resource_id').inTable('resources')
+			table.integer('project_id').unsigned().notNullable()
+				.references('project_id').inTable('projects')
+		})
+};
+
+exports.down = async function (knex) {
+	await knex.schema
+		.dropTableIfExists('project_resources')
+		.dropTableIfExists('tasks')
+		.dropTableIfExists('resources')
+		.dropTableIfExists('projects')
+};
